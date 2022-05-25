33317e8bef4ff572dd6b4610999b65ff68852d4b
Dylan Malla <51140000+D-Malla@users.noreply.github.com>
2022-05-24 21:16:13 -0700

    Added DCharacter & Setup input
    -Added Character class
    -Added MoveForward/ Turn input

---
+++ Source/ThirdPersonAction/ThirdPersonActionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonActionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONACTION_API AThirdPersonActionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
